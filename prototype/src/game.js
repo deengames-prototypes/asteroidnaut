ed79930229e938f1cce245644368000e9c870bdb
ashes999 <ashes999@yahoo.com>
2015-05-19 18:34:54 -0400

    Almost got infinite backgrounds working

--- prototype/src/game.js
+++ prototype/src/game.js
@@ -7,7 +7,17 @@ Game = {
 
   start: function() {
     Crafty.init(Game.view.width, Game.view.height);
-    Crafty.background('black');
+
+    Crafty.e('Background').bind('ViewportScroll', function() {
+      this.y = 0 - (Math.round(Crafty.viewport.y / 600) * 600);
+      console.log("B1 is at " + this.y);
+    });
+
+    Crafty.e('Background').bind('ViewportScroll', function() {
+      this.y = 600 - (Math.round(Crafty.viewport.y / 600) * 600);
+      console.log("B2 is at " + this.y);
+    });
+
     var player = Crafty.e('Player').move(150, 350);
     Crafty.e('Asteroid').move(150, 400);
     Crafty.e('Destruction');
@@ -20,13 +30,20 @@ Game = {
     // Right wall
     Crafty.e('Actor, Wall').size(1, 600).move(300, 0)
       .bind('ViewportScroll', function() {
-        this.y = -Crafty.viewport.y;
+        this.y = -(Crafty.viewport.y / 2);
       });
     Crafty.viewport.follow(player);
   }
 }
 
 window.addEventListener('load', Game.start);
+
+Crafty.c('Background', {
+  init: function() {
+    this.requires('Actor, Image').size(300, 600)
+      .image('images/background.jpg', 'repeat-y');
+  }
+});
 
 Crafty.c('Player', {
   init: function() {
